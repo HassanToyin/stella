f4f86ab548c4bb6b12488116b7f6fc4fd7068fe0
toyin <toyinrasheedat74@gmail.com>
2023-09-15 22:06:16 +0100

    modifyb files

---
+++ index.js
@@ -0,0 +1,81 @@
+// alert("motivation is key")
+// document.write("hello septa people")
+// console.log("life requires motivation")
+
+// login code
+console.log("motivation");
+
+const name = "silvia"
+
+// const name = "femi"
+
+let fish;
+
+fish = "titus"
+fish = "catfish"
+
+console.log(fish);
+
+function dosomething() {
+    console.log(name)
+}
+
+//Comment
+
+//statement
+
+//variable
+
+//var
+//let
+//const
+
+const church = "deeper life";
+
+let name1 
+
+console.log(church);
+
+
+// data types
+
+// 1. string
+let sentence = "i wnt to the market";
+
+// 2. Number;
+
+let age = "20"
+let newage = "10"
+
+console.log(age + newage);
+
+//concertenation
+
+// 4. Boolean
+
+let isfemiawake = false
+
+// typeof
+
+console.log(typeof (age));
+
+//concertenation
+
+const website = "google";
+const url = "https://www." +website+ ".com"
+
+console.log(url)
+
+const oldname = "bayo";
+const oldage = 28
+
+console.log("i know" + oldname + "is" + oldage + "years old");
+
+const namee =  "hassan toyin";
+const address = "ikeja";
+const school = "LASU";
+
+
+console.log(" my name is "  + namee +  " i live at "  + address + " i attended "  +  school);
+
+
