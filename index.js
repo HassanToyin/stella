2134f79a7507add6131f4e15c05a98fa7f705605
toyin <toyinrasheedat74@gmail.com>
2023-09-16 14:36:11 +0100

    modify files

--- index.js
+++ index.js
@@ -78,4 +78,71 @@ const school = "LASU";
 
 console.log(" my name is "  + namee +  " i live at "  + address + " i attended "  +  school);
 
+//params or argument or placeholder
+
+function sum(x) {
+    console.log(2 + x);
+}
+
+sum(5) // invoke or call
+
+function addition(a,b) {
+    console.log(a + b)
+}
+
+
+addition(2,2)
+addition(3,2)
+addition(4,2)
+addition(5,2)
+
+let sum1 = 2+2
+let sum2 = 3+2
+let sum3 = 4+2
+let sum4 = 5+2
+
+
+// Nigeria NIG
+
+function country(country, CountryAbbreviation) {
+    console.log(country + " " + CountryAbbreviation)
+}
+
+country("Nigeria","Nig")
+country("Ghana","Gha")
+country("London","Lon")
+country("Canada","Can")
+country("Paris","Par")
+
+function sum(a, b) {
+    console.log(a + b)
+    let result = a+b
+    console.log(a + " + " + b + " = " + result) 
+}
+
+sum(2,1)
+sum(2,2)
+
+
+function sub(a, b) {
+    console.log(a-b)
+    let result = a-b
+    console.log(a + "-" + b + " = " + result)
+}
+
+
+sub(2,1)
+sub(3,2)
+
+
+
+
+function multiply(a, b){
+    console.log(a + " x " + b + " = " + a*b)
+}
+
+multiply(2, 1)
+multiply(3, 2)
+
+
 
